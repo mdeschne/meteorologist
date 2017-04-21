11b2aa7e3fa9cfc9bda1fe491d9dbfafd87ba1c6
mdeschne <mdeschne@users.noreply.github.com>
2017-04-21 00:54:32 -0500

    i hate this pos

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -1,5 +1,7 @@
 require 'open-uri'
 
+
+
 class GeocodingController < ApplicationController
   def street_to_coords_form
     # Nothing to do here.
@@ -9,17 +11,19 @@ class GeocodingController < ApplicationController
   def street_to_coords
     @street_address = params[:user_street_address]
 
-    # ==========================================================================
-    # Your code goes below.
-    #
-    # The street address that the user typed is in the variable @street_address.
-    # ==========================================================================
 
 
+    url = "http://maps.googleapis.com/maps/api/geocode/ison?address="
 
-    @latitude = "Replace this string with your answer."
+    url_useredit = url + @street_address.gsub(" ","+")
 
-    @longitude = "Replace this string with your answer."
+    parsed_data = JSON.parse(open(url_useredit).read)
+    latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+
+    @latitude = latitude
+
+    @longitude = longitude
 
     render("geocoding/street_to_coords.html.erb")
   end
